692365665fcd74d20d789b4e25b3984c2f873203
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-07-05 14:44:01 +0000

    random math function set to generate random card

--- src/app.js
+++ src/app.js
@@ -6,7 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  Int [] icon = [♦,♥,♠,♣];
-  Int [] value=[2,3,4,5,6,7,8,9,10];
-  console.log("Hello Rigo from the console!");
+  let cardElement= document.getElementById("icon");
+  let cardElement= document.getElementById (number);
+  cardElement.innerHTML= generateCard ();
+  console.log(generateCard);
 };
+function generateCard(){
+  var icon= [♦,♥,♠,♣];
+  var number=[2,3,4,5,6,7,8,9,10];
+  let randomIcon = icon[Math.floor(Math.random() * icon.length)];
+  let randomNumber = number[Math.floor(Math.random() * number.length)];
+  let card=window.onload;
+  return window.onload;
+}
+console.log(window.onload);
