8834a4a61ea1948adb420f02630582490852f2ce
Deucalino Marildo Muvuma <muvumafernando@gmail.com>
2023-09-12 22:31:17 +0000

    almost done

--- src/app.js
+++ src/app.js
@@ -4,19 +4,32 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const icons = ["♥️", "♦️", "♠️", "♣️"];
+const card = document.getElementById("card");
 
-window.onload = function() {
-  let cardElement= document.getElementById("icon");
-  let cardElement= document.getElementById (number);
-  cardElement.innerHTML= generateCard ();
-  console.log(generateCard);
-};
-function generateCard(){
-  var icon= [♦,♥,♠,♣];
-  var number=[2,3,4,5,6,7,8,9,10];
-  let randomIcon = icon[Math.floor(Math.random() * icon.length)];
-  let randomNumber = number[Math.floor(Math.random() * number.length)];
-  let card=window.onload;
-  return window.onload;
+const numberElement = document.getElementById("value");
+const iconElement = document.getElementById("icon");
+function generateCard() {
+  const randomNumbers = Math.floor(Math.random() * numbers.length);
+  const randomIcons = Math.floor(Math.random() * icons.length);
+  const number = numbers[randomNumbers];
+  const icon = icons[randomIcons];
 }
-console.log(window.onload);
+const changeCardButton = document.getElementById("changeCard");
+changeCardButton.addEventListener("click", generateCard);
+console.log(generateCard);
